20266fec435eeb1af3b297e900783d2b9d3e6858
Unknown <leechinghon@gmail.com>
2019-01-14 02:49:53 +0800

    banner animation completed

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: require.resolve('snapsvg/dist/snap.svg.js'),
+          use: 'imports-loader?this=>window,fix=>module.exports=0',
+        },
+      ],
+    },
+    resolve: {
+      alias: {
+        snapsvg: 'snapsvg/dist/snap.svg.js',
+      },
+    },
+  }
+}
