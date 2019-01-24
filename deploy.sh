851a0ea4e7ab46d7389b7300c7555e4d5fd4fb25
Unknown <leechinghon@gmail.com>
2019-01-25 00:33:08 +0800

    update for custom domain

--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,20 @@ set -e
 # build
 npm run build
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# navigate into the build output directory
+cd dist
 
-git checkout gh-pages
-git add -f dist
-git commit -m 'deploy' --no-verify
+# if you are deploying to a custom domain
+echo 'xxlee.cc' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git subtree push --prefix dist origin gh-pages
+git push -f git@github.com:xxlee0927/xxlee-portfolio.git master:gh-pages
+
 cd -
